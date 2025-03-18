d3120d38f077e428d52b13c2ca2c1cd4f468456b
Victor Jordão <vitinjordao1@gmail.com>
2025-03-18 14:23:03 -0300

    .

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,9 +1,10 @@
 import img1 from "../assets/img1.jpeg"
+import proj1 from "../assets/projeto1.jpg"
 
 export const ProjectList = [
     {
         name: "Login Screen",
-        image: img1,
+        image: proj1,
         skills: "MongoDB, ExpressJS, Node",
         link: "https://frontend-login-e2us.vercel.app",
         description: "This simple project implements a login screen using MongoDB, ExpressJS, and Node. It provides secure authentication with access to a backend API, which interacts directly with the MongoDB database to store and retrieve user information efficiently."
