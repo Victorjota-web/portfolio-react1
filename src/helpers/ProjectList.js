d1c2512bb7e975b42a3b2dab303a0e3457f11a3e
Victor Jordão <vitinjordao1@gmail.com>
2025-03-18 14:21:27 -0300

    .

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -6,12 +6,12 @@ export const ProjectList = [
         image: img1,
         skills: "MongoDB, ExpressJS, Node",
         link: "https://frontend-login-e2us.vercel.app",
-        description: "Este projeto implementa uma tela de login utilizando MongoDB, ExpressJS e Node. Ele oferece autenticação segura com acesso a uma API backend, que interage diretamente com o banco de dados MongoDB para armazenar e recuperar informações dos usuários de maneira eficiente."
+        description: "This simple project implements a login screen using MongoDB, ExpressJS, and Node. It provides secure authentication with access to a backend API, which interacts directly with the MongoDB database to store and retrieve user information efficiently."
     },
     {
-        name: "Login Screen",
+        name: "E-commerce Screen",
         image: img1,
         skills: "React",
-        description: "Este projeto utiliza React para criar uma interface interativa para a tela de login. Ele se comunica com uma API backend que permite realizar o login e o registro de usuários, com persistência dos dados no banco de dados MongoDB."
+        description: "."
     }
 ]
