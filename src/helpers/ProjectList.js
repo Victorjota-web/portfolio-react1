6d8c779d774806d8daa70ccf7d644ebecd849fb7
Victor Jordão <vitinjordao1@gmail.com>
2025-03-19 15:20:19 -0300

    .

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -10,7 +10,7 @@ export const ProjectList = [
         description: "This simple project implements a login screen using MongoDB, ExpressJS, and Node. It provides secure authentication with access to a backend API, which interacts directly with the MongoDB database to store and retrieve user information efficiently."
     },
     {
-        name: "E-commerce Screen",
+        name: "Pizza Website",
         image: img1,
         skills: "React",
         description: "."
