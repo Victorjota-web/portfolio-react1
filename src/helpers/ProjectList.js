6cdf948828efad4ae34f88dbf9a06cab24e8e3cf
Victor Jordão <vitinjordao1@gmail.com>
2025-03-19 15:38:08 -0300

    .

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,4 +1,4 @@
-
+import img1 from "../assets/img1.jpeg"
 import proj1 from "../assets/projeto1.jpg"
 import proj2 from "../assets/projeto2.jpg"
 
@@ -16,5 +16,12 @@ export const ProjectList = [
         skills: "ReactJS",
         link: "https://pizza-website-tawny-seven.vercel.app/",
         description: "This is a responsive pizza ordering website built with React, featuring sections like Home, Menu, About, and Contact. It doesn't integrate with a backend but serves as a solid project for practicing React Hooks and other core React concepts."
+    },
+    {
+        name: "Website",
+        image: proj2,
+        skills: "ReactJS NodeJS",
+        link: "",
+        description: "."
     }
 ]
